f457ce2b8f87ca4a7f6eb90f4e64bebc259d2d6c
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-11-12 18:57:02 +0600

    Add Fetchjson App

---
+++ fetchjson/index.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then((res) => {
+    const todo = res.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    The Todo with ID: ${id}
+    Has title of: ${title}
+    Is is finished? ${completed}
+`);
+};
