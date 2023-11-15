9f81a69a10c11c4401df5e86d696b3344f03f9ad
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-11-15 18:42:24 +0600

    Add Annotation Rules

---
+++ features/annotation/variables.ts
@@ -0,0 +1,48 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = false;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "yellow"];
+let myNumber: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// classes
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20,
+};
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+};
+
+// when to use annotations
+// 1) function that returns the any type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2 ) when we declare a variable on one line and initialize it later
+
+// let fruit; // problematic
+let fruit: boolean; // solution
+
+const array = ["red", "green", "yellow", "orange"];
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] === "orange") {
+        fruit = true;
+    }
+}
